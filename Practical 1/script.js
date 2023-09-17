12ceed1f31789a5219abf12d663340ab42525262
Oleh Liubchenko <doroleijn@gmail.com>
2023-09-17 21:19:45 +0200

    Practical 1

---
+++ Practical 1/script.js
@@ -0,0 +1,64 @@
+/* Завдання 1 */
+alert("Я JavaScript!");
+
+/* Завдання 2 */
+var str = 'Oleh';
+alert(str[0]);
+
+/* Завдання 3 */
+let num = 3;
+alert(num);
+
+let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+let c = 15;
+let d = 2;
+let result;
+result = b + c;
+alert(result);
+
+let a = 10;
+let b = 2;
+let c = 5;
+alert(a + b + c);
+
+/* Завдання 4 */
+var str = 'Добрий день!';
+alert(str);
+
+var str1 = 'Добрий';
+var str2 = 'день!';
+alert(str1 + ' ' + str2);
+
+var name = 'Oleh';
+alert('Привіт, ' + name + '!');
+
+let age = 17;
+alert('Мені ' + age + 'років!');
+
+/* Завдання 5 */
+var name = prompt("Яке ваше ім'я?");
+alert("Ваше ім'я: " + name);
+
+let number = prompt("Введіть число: ");
+alert( number **2);
+
+/* Завдання 6 */
+var num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+
+/* Завдання 7 */
+var num = 10;
+num++;
+num++;
+num--;
+alert(num);
